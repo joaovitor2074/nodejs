42d3552c6b5fca44a154be87483f9e827e373d28
Joao vitor <jvzinn2074@gmail.com>
2025-02-19 09:10:25 -0300

    hmm hello

--- http/app.js
+++ http/app.js
@@ -1,5 +1,21 @@
-import http from 'http';
-http.createServer(function(req,res){
-    res.end("hello world")
-}).listen(2074);
+import express from "express";
+const app = express()
 
+app.get("/", (req,res)=>{
+    res.send("jv lindo")
+})
+
+app.get("/sobre",(req,res)=>{
+    res.send("minha pagina sobre")
+})
+
+app.get("/blog",(req,res)=>{
+    res.send("bem vindo ao meu blog")
+})
+
+
+
+
+app.listen(2074,()=>{
+    console.log("servidor rodando")
+})
